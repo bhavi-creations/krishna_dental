bd3c40365fe6a8bf25accbc1614c574b0dba253d
Ramesh92340 <rameshpilli1428@gmail.com>
2025-02-13 15:50:30 +0530

    files added

--- services.php
+++ services.php
@@ -24,108 +24,102 @@
 <!--==============================
     Service Area
     ==============================-->
-<section class="vs-service-wrapper space-top space-md-bottom       " >
+<section class="vs-service-wrapper space-top space-md-bottom       ">
     <img src="assets/img/special/cutie_teeth.png" class="special_teeth img-fluid" alt="">
     <div class="container">
         <div class="row">
 
-            <div class="col-md-6 col-xl-4">
+            <div class="col-md-4 col-lg-3">
                 <div class="service-thumb">
                     <div class="sr-img border">
                         <img class="img-fluid" src="assets/img/about/krishnadentacure_root_canal_service.png" alt="Serivce Image">
                     </div>
                     <div class="sr-body icon_bg">
-                    <span class="sr-icon"><img src="assets/img/service img/root-canal (1).png" alt="" class="icon"></span>
+                        <span class="sr-icon"><img src="assets/img/service img/root-canal (1).png" alt="" class="icon"></span>
 
                         <h3 class="h4 sr-title "><a class="text-reset" href="root.php">Root Canal</a></h3>
                         <div class="sr-content">
-                            <p class="sr-text">Root canal treatment saves infected teeth by removing decay, cleaning roots, and sealing them to prevent further damage. </p>
                             <a href="root.php" class="link-btn">Read More<i class="fal fa-long-arrow-right"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="col-md-6 col-xl-4">
+            <div class="col-md-4 col-lg-3">
                 <div class="service-thumb">
                     <div class="sr-img border">
                         <img class="img-fluid" src="assets/img/about/krishnadentacure_wisdom_toothremoval_sservice.png" alt="Serivce Image">
 
                     </div>
                     <div class="sr-body icon_bg">
-                    <span class="sr-icon"><img src="assets/img/service img/wisdom-tooth.png" alt=""  class="icon"></span>
+                        <span class="sr-icon"><img src="assets/img/service img/wisdom-tooth.png" alt="" class="icon"></span>
 
                         <h3 class="h4 sr-title "><a class="text-reset" href="wisdom.php"> Wisdom Tooth Removal</a></h3>
                         <div class="sr-content">
-                            <p class="sr-text">Wisdom tooth removal extracts impacted or painful teeth to prevent infection, crowding, and discomfort. </p>
                             <a href="wisdom.php" class="link-btn">Read More<i class="fal fa-long-arrow-right"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-4">
+            <div class="col-md-4 col-lg-3">
                 <div class="service-thumb">
                     <div class="sr-img border">
                         <img class="img-fluid" src="assets/img/about/kroshnadentacure_bad_breath_treatment_image.png" alt="Serivce Image">
 
                     </div>
                     <div class="sr-body icon_bg">
-                    <span class="sr-icon"><img src="assets/img/service img/breath.png" alt=""  class="icon"></span>
+                        <span class="sr-icon"><img src="assets/img/service img/breath.png" alt="" class="icon"></span>
 
                         <h3 class="h4 sr-title "><a class="text-reset" href="breath.php">Bad Breath Treatment</a></h3>
                         <div class="sr-content">
-                            <p class="sr-text">Bad breath treatment targets bacteria and plaque buildup through proper oral hygiene, hydration, and professional cleaning. </p>
                             <a href="breath.php" class="link-btn">Read More<i class="fal fa-long-arrow-right"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-4">
+            <div class="col-md-4 col-lg-3">
                 <div class="service-thumb">
                     <div class="sr-img border">
                         <img class="img-fluid" src="assets/img/about/krishnadentacure_gum_treatment_service.png" alt="Serivce Image">
 
                     </div>
                     <div class="sr-body icon_bg">
-                    <span class="sr-icon"><img src="assets/img/service img/dental-surgery.png" alt=""  class="icon"></span>
+                        <span class="sr-icon"><img src="assets/img/service img/dental-surgery.png" alt="" class="icon"></span>
 
                         <h3 class="h4 sr-title "><a class="text-reset" href="gum.php">Gum Treatment</a></h3>
                         <div class="sr-content">
-                            <p class="sr-text">Gum treatment heals infections and reduces inflammation through deep cleaning, medication, and proper oral care. </p>
                             <a href="gum.php" class="link-btn">Read More<i class="fal fa-long-arrow-right"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-4">
+            <div class="col-md-4 col-lg-3">
                 <div class="service-thumb">
                     <div class="sr-img border">
                         <img class="img-fluid" src="assets/img/about/krishnadentacure_teethcleaning__service.png" alt="Serivce Image">
 
                     </div>
                     <div class="sr-body icon_bg">
-                    <span class="sr-icon"><img src="assets/img/service img/teeth.png" alt=""  class="icon"></span>
+                        <span class="sr-icon"><img src="assets/img/service img/teeth.png" alt="" class="icon"></span>
 
                         <h3 class="h4 sr-title "><a class="text-reset" href="cleaning.php">Teeth Cleaning</a></h3>
                         <div class="sr-content">
-                            <p class="sr-text">Teeth cleaning removes plaque, tartar, and stains to maintain oral health and prevent cavities.</p>
                             <a href="cleaning.php" class="link-btn">Read More<i class="fal fa-long-arrow-right"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-4">
+            <div class="col-md-4 col-lg-3">
                 <div class="service-thumb">
                     <div class="sr-img border">
                         <img class="img-fluid" src="assets/img/about/krishnadentacure_Orthodontic_service.png" alt="Serivce Image">
 
                     </div>
                     <div class="sr-body icon_bg">
-                    <span class="sr-icon"><img src="assets/img/service img/dentist.png" alt=""  class="icon"></span>
+                        <span class="sr-icon"><img src="assets/img/service img/dentist.png" alt="" class="icon"></span>
 
                         <h3 class="h4 sr-title "><a class="text-reset" href="orthodontic.php">Orthodontic Treatment</a></h3>
                         <div class="sr-content">
-                            <p class="sr-text">Orthodontic treatment aligns teeth and corrects bite issues using braces, aligners, or retainers for a perfect smile. </p>
                             <a href="orthodontic.php" class="link-btn">Read More<i class="fal fa-long-arrow-right"></i></a>
                         </div>
                     </div>
@@ -134,155 +128,146 @@
 
 
 
-            <div class="col-md-6 col-xl-4">
+            <div class="col-md-4 col-lg-3">
                 <div class="service-thumb">
                     <div class="sr-img border">
                         <img class="img-fluid" src="assets/img/about/krishnadentacure_crownand_bridges.png" alt="Serivce Image">
 
                     </div>
                     <div class="sr-body icon_bg">
-                    <span class="sr-icon"><img src="assets/img/service img/bridge.png" alt=""  class="icon"></span>
+                        <span class="sr-icon"><img src="assets/img/service img/bridge.png" alt="" class="icon"></span>
 
                         <h3 class="h4 sr-title "><a class="text-reset" href="crown.php">Dental Crown & Bridge </a></h3>
                         <div class="sr-content">
-                            <p class="sr-text">Dental crowns and bridges restore damaged or missing teeth, improving function, appearance, and bite alignment. </p>
                             <a href="crown.php" class="link-btn">Read More<i class="fal fa-long-arrow-right"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-4">
+            <div class="col-md-4 col-lg-3">
                 <div class="service-thumb">
                     <div class="sr-img border">
                         <img class="img-fluid" src="assets/img/about/krishnadentacure_dental_Veneers_service.png" alt="Serivce Image">
 
                     </div>
                     <div class="sr-body icon_bg">
-                    <span class="sr-icon"><img src="assets/img/service img/veener.png" alt=""  class="icon"></span>
+                        <span class="sr-icon"><img src="assets/img/service img/veener.png" alt="" class="icon"></span>
 
                         <h3 class="h4 sr-title "><a class="text-reset" href="veneers.php">Dental Veneers</a></h3>
                         <div class="sr-content">
-                            <p class="sr-text">Dental veneers are thin shells that cover teeth to enhance appearance, correct stains, chips, and gaps.</p>
                             <a href="veneers.php" class="link-btn">Read More<i class="fal fa-long-arrow-right"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-4">
+            <div class="col-md-4 col-lg-3">
                 <div class="service-thumb">
                     <div class="sr-img border">
                         <img class="img-fluid" src="assets/img/about/krishnadentacure_smilemakeover_service.png" alt="Serivce Image">
 
                     </div>
                     <div class="sr-body icon_bg">
-                    <span class="sr-icon"><img src="assets/img/service img/smile.png" alt=""  class="icon"></span>
+                        <span class="sr-icon"><img src="assets/img/service img/smile.png" alt="" class="icon"></span>
 
                         <h3 class="h4 sr-title "><a class="text-reset" href="smile.php">Smile Makeover</a></h3>
                         <div class="sr-content">
-                            <p class="sr-text">A smile makeover enhances teeth and gums using whitening, veneers, braces, or crowns for a perfect smile.</p>
                             <a href="smile.php" class="link-btn">Read More<i class="fal fa-long-arrow-right"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-4">
+            <div class="col-md-4 col-lg-3">
                 <div class="service-thumb">
                     <div class="sr-img border">
                         <img class="img-fluid" src="assets/img/about/keishnadentacure_teeth_whitening_service.png" alt="Serivce Image">
 
                     </div>
                     <div class="sr-body icon_bg">
-                    <span class="sr-icon"><img src="assets/img/service img/tooth-whitening.png" alt=""  class="icon"></span>
+                        <span class="sr-icon"><img src="assets/img/service img/tooth-whitening.png" alt="" class="icon"></span>
 
                         <h3 class="h4 sr-title "><a class="text-reset" href="whitening.php">Teeth Whitening</a></h3>
                         <div class="sr-content">
-                            <p class="sr-text">Teeth whitening brightens stained or discolored teeth using bleaching agents for a whiter, radiant smile. </p>
                             <a href="whitening.php" class="link-btn">Read More<i class="fal fa-long-arrow-right"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-4">
+            <div class="col-md-4 col-lg-3">
                 <div class="service-thumb">
                     <div class="sr-img border">
                         <img class="img-fluid" src="assets/img/about/krishnadentacure_invisible_aligners_services.png" alt="Serivce Image">
 
                     </div>
                     <div class="sr-body icon_bg">
-                    <span class="sr-icon"><img src="assets/img/service img/invisligners.png" alt=""  class="icon"></span>
+                        <span class="sr-icon"><img src="assets/img/service img/invisligners.png" alt="" class="icon"></span>
 
                         <h3 class="h4 sr-title "><a class="text-reset" href="invisaliners.php">Invisible Aligners
-                        </a></h3>
+                            </a></h3>
                         <div class="sr-content">
-                            <p class="sr-text">Invisible aligners straighten teeth discreetly using clear, removable trays, enhancing alignment, comfort, and confidence. </p>
                             <a href="invisaliners.php" class="link-btn">Read More<i class="fal fa-long-arrow-right"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-4">
+            <div class="col-md-4 col-lg-3">
                 <div class="service-thumb">
                     <div class="sr-img border">
                         <img class="img-fluid" src="assets/img/about/krishnadentacure_dental_implants_services.png" alt="Serivce Image">
 
                     </div>
                     <div class="sr-body icon_bg">
-                    <span class="sr-icon"><img src="assets/img/service img/implant.png" alt=""  class="icon"></span>
+                        <span class="sr-icon"><img src="assets/img/service img/implant.png" alt="" class="icon"></span>
 
                         <h3 class="h4 sr-title "><a class="text-reset" href="implents.php">Dental Implants</a></h3>
                         <div class="sr-content">
-                            <p class="sr-text">Dental implants replace missing teeth with artificial roots and crowns, restoring function, appearance, and confidence. </p>
                             <a href="implents.php" class="link-btn">Read More<i class="fal fa-long-arrow-right"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-4">
+            <div class="col-md-4 col-lg-3">
                 <div class="service-thumb">
                     <div class="sr-img border">
                         <img class="img-fluid" src="assets/img/about/krishnadentacure_dentures_treatment_services.png" alt="Serivce Image">
 
                     </div>
                     <div class="sr-body icon_bg">
-                    <span class="sr-icon"><img src="assets/img/service img/denture.png" alt=""  class="icon"></span>
+                        <span class="sr-icon"><img src="assets/img/service img/denture.png" alt="" class="icon"></span>
 
                         <h3 class="h4 sr-title "><a class="text-reset" href="dentures.php"> Dentures </a></h3>
                         <div class="sr-content">
-                            <p class="sr-text">Dentures are removable prosthetic teeth that restore function and appearance for those with missing teeth. </p>
                             <a href="dentures.php" class="link-btn">Read More<i class="fal fa-long-arrow-right"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-4">
+            <div class="col-md-4 col-lg-3">
                 <div class="service-thumb">
                     <div class="sr-img border">
                         <img class="img-fluid" src="assets/img/about/krishnadentacure_Fluoride application strengthens _ service.png" alt="Serivce Image">
 
                     </div>
                     <div class="sr-body icon_bg">
-                    <span class="sr-icon"><img src="assets/img/service img/sealant.png" alt=""  class="icon"></span>
+                        <span class="sr-icon"><img src="assets/img/service img/sealant.png" alt="" class="icon"></span>
 
                         <h3 class="h4 sr-title "><a class="text-reset" href="sealant.php"> Fluoride Application & Dental Sealant</a></h3>
                         <div class="sr-content">
-                            <p class="sr-text">Fluoride application strengthens enamel to prevent cavities, while dental sealants protect teeth by sealing deep grooves.</p>
                             <a href="sealant.php" class="link-btn">Read More<i class="fal fa-long-arrow-right"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-4">
+            <div class="col-md-4 col-lg-3">
                 <div class="service-thumb">
                     <div class="sr-img border">
                         <img class="img-fluid" src="assets/img/about/krishnadentacure_fullmouthrestoration_service.png" alt="Serivce Image">
 
                     </div>
                     <div class="sr-body icon_bg">
-                    <span class="sr-icon"><img src="assets/img/service img/dental.png" alt=""  class="icon"></span>
+                        <span class="sr-icon"><img src="assets/img/service img/dental.png" alt="" class="icon"></span>
 
                         <h3 class="h4 sr-title "><a class="text-reset" href="fullmouth.php"> Full Mouth Rehabilitation Treatment</a></h3>
                         <div class="sr-content">
-                            <p class="sr-text">Full mouth rehabilitation restores function and aesthetics using crowns, implants, veneers, and other dental treatments. </p>
                             <a href="fullmouth.php" class="link-btn">Read More<i class="fal fa-long-arrow-right"></i></a>
                         </div>
                     </div>
